fe144af51eb7d145338964be74b9a6a903d8def8
Raimundo Neto <raimundononatobatistaneto@gmail.com>
2024-03-21 12:04:20 -0300

    Add header component structure

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,36 @@
 export function Header() {
     return (
-        <h1>Header</h1>
+        <header>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary p-4" data-bs-theme="dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Lifters Shop</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mb-2 mb-lg-0">
+                            <li className="nav-item me-3">
+                                <a className="nav-link active" aria-current="page" href="#">Shop</a>
+                            </li>
+                            <li className="nav-item me-3">
+                                <a className="nav-link" href="#">Stories</a>
+                            </li>
+                            <li className="nav-item me-3">
+                                <a className="nav-link" href="#">About</a>
+                            </li>
+                        </ul>
+                        <form className="me-auto" role="search">
+                            <div className="input-group">
+                                <span className="input-group-text" id="basic-addon1">
+                                    <i className="bi bi-search"></i>
+                                </span>
+                                <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon1" />
+                            </div>
+                        </form>
+                        <div>
+                            <span className="navbar-text me-5">Carrinho</span>
+                            <button type="button" className="btn btn-outline-light">Login</button>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
