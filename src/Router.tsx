b7d7d709188e5a01f7b674b0802bcdc58b0de8f4
Raimundo Neto <raimundononatobatistaneto@gmail.com>
2024-03-21 01:56:14 -0300

    Add default layout component | Add Header component

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,13 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Details } from './pages/Details';
 import { Checkout } from './pages/Checkout';
+import { DefaultLayout } from './layouts/DefaultLayout';
 
 export function Router() {
     return (
         <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/details' element={<Details />} />
-            <Route path='/checkout' element={<Checkout />}/>
+            <Route path='/' element={<DefaultLayout />}>
+                <Route path='/' element={<Home />} />
+                <Route path='/details' element={<Details />} />
+                <Route path='/checkout' element={<Checkout />}/>
+            </Route>
         </Routes>
     );
 }
