98ccde6354ae74c5b01805e8426ec1f81d0f3257
Raimundo Neto <raimundononatobatistaneto@gmail.com>
2024-03-21 13:00:44 -0300

    Add homepage structure

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,24 @@
 export function Home() {
     return (
         <div>
-            <h1>Home page</h1>
+            <div className="container-fluid bg-dark text-white p-4">
+                <h1 className="mb-2">Shop Men's</h1>
+                <p>
+                    Revamp your style with the latest designer trends in men's<br />
+                    clothing or achieve a perfectly curated wardrobe thanks to our<br /> 
+                    line-up of timeless pieces.
+                </p>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <p>filtros</p>
+                    </div>
+                    <div className="col">
+                        <p>Produtos</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
